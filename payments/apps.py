efe10192be661e321af5568bd7ad4af487db2cdf
Camryn Crowder <camryn.crowder@apkudo.com>
2025-01-25 12:52:44 -0500

    chore: dockerize

--- payments/apps.py
+++ payments/apps.py
@@ -1,6 +1,16 @@
 from django.apps import AppConfig
+from django.db.models.signals import pre_migrate
+from django.db import connection
 
 
 class PaymentsConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
     name = "payments"
+
+    def ready(self):
+        pre_migrate.connect(create_schema, sender=self)
+
+def create_schema(sender, **kwargs):
+    schema_name = "payment_api"
+    with connection.cursor() as cursor:
+        cursor.execute(f"CREATE SCHEMA IF NOT EXISTS {schema_name};")
